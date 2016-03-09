6723e185ef6461df003924e0964f5aecc0b6f3bc
Garth Reckers <garthreckers@Garths-MacBook-Pro.local>
2016-03-08 21:04:54 -0800

    testing post data

--- app.py
+++ app.py
@@ -7,19 +7,23 @@ APP = Flask(__name__)
 if keys.IS_PRODUCTION:
 	APP.debug = True
 
+TEST_POST_PASS = "8392hfuehf934fn93f2dj20djw0d92"
+
 @APP.route("/")
 def hello():
 	return "test"
 
 
-@APP.route("/hashtag/<input_hashtag>")
+@APP.route("/hashtag/<input_hashtag>", methods=['POST'])
 def hash_t(input_hashtag):
+	if request.form['api_key'] = TEST_POST_PASS:
+		c = Cache(input_hashtag)
+		cc = c.buildIt()
+		cr = c.readIt()
 
-	c = Cache(input_hashtag)
-	cc = c.buildIt()
-	cr = c.readIt()
-
-	return cr
+		return cr
+	else:
+		return 403
 
 
 
